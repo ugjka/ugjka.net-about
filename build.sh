56c2f5753c1a4a6cc410d129486f89d956f3bafc
Uģis Ģērmanis <ugis@germanis.lv>
2026-01-20 14:54:08 +0200

    bundler

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 nix shell github:bobvanderlinden/nixpkgs-ruby#'"ruby-2.7"' --extra-experimental-features nix-command --extra-experimental-features flakes --command \
+bundle config set --local deployment 'true'
+
+nix shell github:bobvanderlinden/nixpkgs-ruby#'"ruby-2.7"' --extra-experimental-features nix-command --extra-experimental-features flakes --command \
+gem install bundler:2.2.25
+
+nix shell github:bobvanderlinden/nixpkgs-ruby#'"ruby-2.7"' --extra-experimental-features nix-command --extra-experimental-features flakes --command \
+bundle install
+
+nix shell github:bobvanderlinden/nixpkgs-ruby#'"ruby-2.7"' --extra-experimental-features nix-command --extra-experimental-features flakes --command \
 bundle exec jekyll build
+
 find _site \( -name '*.css' -o -name '*.html' -o -name '*.js' -o -name '*.json' -o -name '*.txt' -o -name '*.xml' -o -name '*.yml' \) -exec gzip --best --keep -f {} \;
 rsync -Pvhr --delete _site/ server.internal:share/about/
